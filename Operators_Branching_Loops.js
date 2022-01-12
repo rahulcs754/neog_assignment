10ffb5716fdcaff5942665a37f3dc15da356945b
rahul shukla <rahul.shukla754@gmail.com>
2022-01-12 22:49:46 +0530

    add:file

--- Operators_Branching_Loops.js
+++ Operators_Branching_Loops.js
@@ -1,48 +1,86 @@
 //Easy
-
 //Write a program to add 5 numbers. The value of numbers are num1=5, num2=13, num3=7, num4=21 and num5=48.
+let sum = 0;
+const sumNum = (...arg) => { 
+   for( item of arg ){
+     sum = sum + item;
+   }
+   return sum;
+ }
 
-
-
+console.log(sumNum(1,2,3,4,5,6));
+     
 //Write a program to take a number input from user and determine whether the number is odd or even.
 
-const askInput = prompt("Please enter number");
+//const askInput = prompt("Please enter number");
+const askInput = 5;  
 const result = askInput % 2 == 0 ? 'even' :'odd';
 console.log(result);
 
 
 //Write a program to find the maximum and minimum out of two given numbers. The numbers are num1=129 and num2=251.
-const findMax = (num1,num2) => num1 > num2 ? ` ${num1} is greater than  ${num2} `:`${num2} is greater than  ${num1} `
+//const findMax = (num1,num2) => num1 > num2 ? ` ${num1} is greater than  ${num2} `:`${num2} is greater than  ${num1} `
 
 //Write a program to find the maximum out of three given numbers. The numbers are num1=8, num2=23 and num3=17.
-const findMax = (num1,num2,num3) => {
+/*const findMax = (num1,num2,num3) => {
     if( num1 > num2 && num1 > num3 ){
-      console.log(` ${num1} is greater than to all `);
+      return num1;
     }else if( num2 > num1 && num2 > num3  ){
-      console.log(` ${num2} is greater than to all `);
+      return num2;
     }else{
-     console.log(` ${num3} is greater than to all `);
+      return num3;
     }
-}
+  }
+*/
+/*  const findMax = (num1,num2,num3) => {
+  num1 = num1 > num2 ? num1 :num2;
+   num2 = num2 > num3 ? num2 :num3;
+   return num2 > num3 ? num2 :num3;
+}*/
 
 //Write a program to find the minimum out of three given numbers. The numbers are num1=35, num2=29 and num3=46.
+/*
 const findMin = (num1,num2,num3) => {
     if( num1 < num2 && num1 < num3 ){
-      console.log(` ${num1} is smaller than to all `);
+      return num1;
     }else if( num2 < num1 && num2 < num3  ){
-      console.log(` ${num2} is smaller than to all `);
+      return num2;
     }else{
-     console.log(` ${num3} is smaller than to all `);
+      return num3;
     }
 }
+*/
 
 //Write program to take a month as an input from the user and find out whether the month has 31 days or not.
-
+/**
 const passMonth = askMonth => ( askMonth !== 2 || askMonth !== 4 || askMonth !==6 || askMonth !== 9 || askMonth !== 11  ) ? 'Given month has 31 days':'Given month has not 31 days' 
 const askMonth = prompt('Please Enter month');
 const result = passMonth(askMonth);
+*/
+
+/* 
+Intermediate Question
+*/
+/*
+//Fizzbuzz - Write a program to return an array from 1 to 100. But for every multiple of 3, replace the number with "Fizz", for every multiple of 5, replace the number with "Buzz" and for every multiples of 3 & 5, replace with "FizzBuzz".
+*/
+ let n = 101;
+ let st = '';
+ while( n > 0 ) {
+
+  if( n % 3  == 0 ){
+   st = ', Fizz';
+  }else if( n%5 == 0 ){
+  st = ', Buzz';
+  }else{
+   st = n+' ';
+  }
+
+   n-1;
+ }
+
+console.log(st);
 
 
-//
 
- 
+
